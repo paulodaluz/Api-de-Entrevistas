ac4534670b82dba7df2ef4b93c475f98967320b5
Paulo da Luz <paulo.daluz@compasso.com.br>
2019-04-01 11:48:48 -0300

    Metodo deleta intrevista criado

--- src/controller/InterviewController.ts
+++ src/controller/InterviewController.ts
@@ -3,6 +3,7 @@ import {Request, Response} from "express";
 import { Error } from '../models/Errors'
 import { getManager } from "typeorm";
 import { Interview } from "../entity/Interview";
+import { InterviewDeleted } from "../entity/InterviewDeleted"
 
 
 var err = new Error();
@@ -21,7 +22,31 @@ export async function newInterview (request: Request, response: Response) {
 }
 
 export async function deleteInterview(request: Request, response: Response) {
-    response.send("ok")
+    //Cria uma conexão com o banco
+    const interviewRepository = getManager().getRepository(Interview);
+
+    //Acha o cliente no banco de dados e guarda na variavel dados cliente
+    const dataInterview = await interviewRepository.findOne(request.params.id);
+
+    //Se o cliente não for encontrado irá retornar o erro padrão ao usuário
+    if (!dataInterview) {
+        response.status(404).json("")
+        response.send();
+        return;
+    }
+    //Deleta o cliente e retorna uma mensagem de sucesso ao usuário
+    await interviewRepository.delete({ id: request.params.id });
+
+    const interviewDeletedRepository = getManager().getRepository(InterviewDeleted);
+    
+    var deletedInterview = await interviewDeletedRepository.create(dataInterview);
+
+    deletedInterview.usuarioExcluidor = request.body.entrevistador;
+
+    await interviewDeletedRepository.save(deletedInterview);
+
+    response.send("Entrevista excluída com sucesso");
+    console.log("Changed table interview successfully!");
 }
 
 export async function searchAllInterview(request: Request, response: Response) {
